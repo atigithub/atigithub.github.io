322207324b049c74194a6ea031787b75f4da8f22
Andrei <andreibmsg@yahoo.com>
2018-04-17 06:50:51 +0300

    Initial Commit

---
+++ JS/simple.js
@@ -0,0 +1,12 @@
+function scrollToX (param) {
+
+    document.getElementById(param).scrollIntoView({
+            behavior: 'smooth'
+        });
+}
+
+
+function myFunction() {
+    document.getElementById("demo").style.color = "red";
+}
+
